5ba5401a053f4c8604bdf947483ed7eb80663a85
mikolaj-kowal <mikolaj.kowal@yahoo.pl>
2025-09-28 09:05:12 +0200

    Add SQL data cleaning script template

---
+++ data_cleaning.sql
@@ -0,0 +1,29 @@
+/*
+=====================================================
+ Project:   e-commerce-sales-analysis
+ Script:    fix_incorrect_dates.sql
+ Purpose:   Clean and fix database data issues
+ Author:    Mikołaj Kowal
+ Date:      2025-09-27
+ Notes:
+ 	- No rows returned by the SELECT statements indicates no inconsistencies or issues were found
+ 	- To perform the check, uncomment the SELECT statements
+=====================================================
+*/
+
+USE ecommerce_db;
+
+
+
+
+/*
+=====================================================
+
+The following statements perform necessary updates to clean and standardize the dataset
+
+=====================================================
+*/
+
+
+
+
